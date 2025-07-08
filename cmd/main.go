4b6b54d029d07a046663d5726b700bb0c2db0a18
Ruslan Kladko <rkladko@gmail.com>
2025-07-08 12:23:08 +0200

    combining video and audio for output

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-	// "io"
+	"io"
 	"log/slog"
 	"os"
+	"os/exec"
 
 	"github.com/rooslunn/myd/youtube"
 )
 
+const (
+	BIN_BASH = "/usr/bin/bash"
+)
+
+const (
+	E_NOT_ALL_ARGS = iota + 1
+	E_EXTRACT_VID
+	E_VIDEO
+	E_COMBINE
+)
 
 func main() {
 	
@@ -18,39 +30,49 @@ func main() {
 
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: go run main.go <youtube_url> <output_file>")
-		os.Exit(1)
+		os.Exit(E_NOT_ALL_ARGS)
 	}
 
 	videoURL := os.Args[1]
-	// outputFile := os.Args[2]
+	outputFile := os.Args[2]
 
 	log := setupLogger()
 
 	videoID, err := youtube.ExtractVideoID(videoURL)
 	if err != nil {
 		fmt.Println("Can't get video id from url")
-		os.Exit(2)
+		os.Exit(E_EXTRACT_VID)
 	}
 
 	client := youtube.NewClient()
 	video, err := client.GetVideo(videoID)
+	videoUniqueName := titleMD5(video.Title)
 	if err != nil {
 		log.Error(err.Error())
+		os.Exit(E_VIDEO)
 	}
 
 	log.Info("Comms clear", "title", video.Title, "videoID", videoID)
 
-	videoFormats := video.Formats.VideoFormats()
-	audioFormats := video.Formats.AudioFormats()
-	videoFormatId := askForFormat(videoFormats) 
-	audioFormatId := askForFormat(audioFormats) 
-	videoFormat := videoFormats[videoFormatId]
-	audioFormat := audioFormats[audioFormatId]
+	videoFormat := askForFormat(video.Formats.VideoFormats()) 
+	audioFormat := askForFormat(video.Formats.AudioFormats()) 
 
-	err = client.DownloadFormat(video, videoFormat, "video.mp4", log)
-	err = client.DownloadFormat(video, audioFormat, "audio.mp4", log)
+	tempVideoFile := fmt.Sprintf("%s_v_%s", videoUniqueName, outputFile)
+	tempAudioFile := fmt.Sprintf("%s_a_%s", videoUniqueName, outputFile)
 
-	log.Info("App operations completed. Godshow.")
+	err = client.DownloadFormat(video, videoFormat, tempVideoFile, log)
+	err = client.DownloadFormat(video, audioFormat, tempAudioFile, log)
+
+	log.Info("Combining video and audio", "video", tempVideoFile, "audio", tempAudioFile)
+
+	cmd := exec.Command(BIN_BASH, "combine.sh", tempVideoFile, tempAudioFile, outputFile)
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Error(err.Error())
+		os.Exit(E_COMBINE)
+	}
+
+	log.Info("All operations completed. Godshow.")
 }
 
 func setupLogger() *slog.Logger {
@@ -58,7 +80,7 @@ func setupLogger() *slog.Logger {
 	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 }
 
-func askForFormat(formats []youtube.Format) (uint8) {
+func askForFormat(formats []youtube.Format) (youtube.Format) {
 	for i, v := range formats {
 		fmt.Printf("%d. %s (%s) %s \n", i+1, v.Quality, v.MimeType, youtube.FormatBytes(v.ContentLength))
 	}
@@ -68,9 +90,11 @@ func askForFormat(formats []youtube.Format) (uint8) {
 	var formatId uint8
 	fmt.Scanln(&formatId)
 
-	return formatId - 1
+	return formats[formatId-1]
 }
 
-func downloadFormat(format youtube.Format) {
-
+func titleMD5(title string) string {
+	h := md5.New()
+	io.WriteString(h, title)
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
